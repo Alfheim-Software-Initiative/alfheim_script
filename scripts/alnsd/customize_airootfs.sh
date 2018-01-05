10bdee6ba3c293fc9eba11aae0b0dd4014a51903
Alexander Maikoda <ze.angry.wombat@gmail.com>
2018-01-05 03:21:16 -0600

    Full rewrite of alnsd - tested and works.

---
+++ scripts/alnsd/customize_airootfs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e -u
+
+sed -i 's/#\(en_US\.UTF-8\)/\1/' /etc/locale.gen
+locale-gen
+
+ln -sf /usr/share/zoneinfo/UTC /etc/localtime
+
+usermod -s /usr/bin/zsh root
+chmod 700 /root
+
+sed -i 's/#\(PermitRootLogin \).\+/\1yes/' /etc/ssh/sshd_config
+sed -i "s/#Server/Server/g" /etc/pacman.d/mirrorlist
+
+chmod 777 /etc/pacman.d/mirrorlist
+chmod 777 /etc/pacman.d/mirrorlist-arch
+chmod 777 /etc/pacman.d/mirrorlist-artix
+
+groupadd sudo
+groupmod -g 900 sudo
+echo "root:toorpassword" | chpasswd
+useradd -d /home/archlive -g wheel archlive
+echo "archlive:archlive" | chpasswd
+usermod -aG sudo archlive
+
+pacman-key --init
+pacman-key --populate artix
+pacman-key --populate archlinux
+
+chown -R archlive /home/archlive
+
+echo " " >> /etc/sudoers
+echo "archlive ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
+
+su archlive -c "yaourt -Sy base-devel auto-auto-complete texman netcfg-git google-chrome-dev yaourt-gui kodi-platform atom-editor-bin gitg-git zpaq obmenu-generator"
---
+++ scripts/alnsd/pre-config.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/bash
+
+/usr/bin/packages
+
+## Add the archlive user home for AUR access
+mkdir -p /home/archlive/{Desktop,Downloads,Music,Pictures,Public,Templates,Videos}
+
+## Add the right items to startup so we have access to our hardware
+rc-update add acpid default
+rc-update add alsasound default
+rc-update add autofs default
+rc-update add cronie default
+rc-update add cupsd default
+rc-update add xdm default
+rc-update add fuse default
+rc-update add haveged default
+rc-update add hdparm default
+rc-update add smb default
+rc-update add sshd default
+rc-update add syslog-ng default
+rc-update add udev boot
+rc-update add elogind boot
+rc-update add dbus boot
+
+## Generate pacman keyrings
+pacman-key --init
+pacman-key --populate artix
+pacman-key --populate archlinux
+update-ca-trust
+
+## mkinitcpio runs here
+cp /usr/lib/initcpio/hooks/archiso /etc/initcpio/hooks
+cp /usr/lib/initcpio/install/archiso /etc/initcpio/install
+cp /usr/lib/initcpio/hooks/archiso_shutdown /etc/initcpio/hooks
+cp /usr/lib/initcpio/install/archiso_shutdown /etc/initcpio/install
+cp /usr/lib/initcpio/hooks/archiso_pxe_common /etc/initcpio/hooks
+cp /usr/lib/initcpio/install/archiso_pxe_common /etc/initcpio/install
+cp /usr/lib/initcpio/hooks/archiso_pxe_nbd /etc/initcpio/hooks
+cp /usr/lib/initcpio/install/archiso_pxe_nbd /etc/initcpio/install
+cp /usr/lib/initcpio/hooks/archiso_pxe_http /etc/initcpio/hooks
+cp /usr/lib/initcpio/install/archiso_pxe_http /etc/initcpio/install
+cp /usr/lib/initcpio/hooks/archiso_pxe_nfs /etc/initcpio/hooks
+cp /usr/lib/initcpio/install/archiso_pxe_nfs /etc/initcpio/install
+cp /usr/lib/initcpio/hooks/archiso_loop_mnt /etc/initcpio/hooks
+cp /usr/lib/initcpio/install/archiso_loop_mnt /etc/initcpio/install
+
+sed -i "s|/usr/lib/initcpio/|/etc/initcpio/|g" /etc/initcpio/install/archiso_shutdown
+cp /usr/lib/initcpio/install/archiso_kms /etc/initcpio/install
+cp /usr/lib/initcpio/archiso_shutdown /etc/initcpio
+
+mkinitcpio -c /etc/mkinitcpio-archiso.conf -k /boot/vmlinuz-linux -g /boot/alfheim.img
