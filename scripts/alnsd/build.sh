87d806fd6667dd66a9f285a7f7b12e3f6fa2b58f
Alexander Maikoda <ze.angry.wombat@gmail.com>
2018-02-23 23:46:31 -0600

    Moved airootfs cleanup to the end

--- scripts/alnsd/build.sh
+++ scripts/alnsd/build.sh
@@ -117,8 +117,13 @@ umount -d ${work_dir}/efiboot
 cp -a -l -f ${work_dir}/airootfs ${work_dir}
 setarch ${arch} mkarchiso ${verbose} -w "${work_dir}" -P "${iso_publisher}" -A "${iso_application}"  -D "${install_dir}" pkglist
 setarch ${arch} mkarchiso ${verbose} -w "${work_dir}" -P "${iso_publisher}" -A "${iso_application}"  -D "${install_dir}" ${gpg_key:+-g ${gpg_key}} prepare
-rm -rf ${work_dir}/airootfs
+#rm -rf ${work_dir}/airootfs
 # rm -rf ${work_dir}/${arch}/airootfs (if low space, this helps)
 
 # Build ISO
 mkarchiso ${verbose} -w "${work_dir}" -D "${install_dir}" -L "${iso_label}" -P "${iso_publisher}" -A "${iso_application}"  -o "${out_dir}" iso "${iso_name}-${iso_version}-x86_64.iso"
+
+## Clean up the directories
+umount ${work_dir}/airootfs
+rm -rf ${work_dir}/airootfs
+rm -rf rootfs
